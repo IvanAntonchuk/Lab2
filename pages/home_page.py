86951a40793e4f027df81eeddfd0806ac7e72f8b
Ivan Antonchuk <mmeisonan@gmail.com>
2026-04-29 06:33:27 +0300

    Add final 3 tests for language, filter, and map

--- pages/home_page.py
+++ pages/home_page.py
@@ -5,9 +5,15 @@ class HomePage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
         self.base_url = "https://lun.ua/uk/"
+        self.SEARCH_INPUT = (By.NAME, "query")
+        self.SEARCH_BUTTON = (By.CSS_SELECTOR, "button[type='submit']")
 
     def open(self):
         self.driver.get(self.base_url)
+
+    def search_for(self, text):
+        self.js_type(self.SEARCH_INPUT, text)
+        self.js_click(self.SEARCH_BUTTON)
 
     def go_to_city_results(self, city_name):
         """Прямий перехід до результатів пошуку по місту"""
